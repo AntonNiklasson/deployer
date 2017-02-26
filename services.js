b4cdc3bfae5c0004cdadacb1482d95815a4efc34
Anton Niklasson <niklasson.anton@gmail.com>
2017-02-26 12:47:09 +0100

    Can this deploy itself?

--- services.js
+++ services.js
@@ -1,9 +1,14 @@
 module.exports = {
+	"deployer": {
+		"path": "/Users/anton/Desktop/test-deployer",
+		"repo": "git@github.com:AntonNiklasson/deployer.git",
+		"commands": [
+			"npm install"
+		]
+	},
 	"bowtie-api": {
-		// path: "/var/www/api.mybowtie.co",
 		path: "/Users/anton/Desktop",
-		repo: "git@github.com:AntonNiklasson/bowtie-api",
-		branch: "master",
+		repo: "git@github.com:AntonNiklasson/bowtie-api.git",
 		commands: [
 			"npm install",
 			"npm test"
