6cc3099ae9fecfd5d833533ec01eb9d15bb736ad
Anton Niklasson <niklasson.anton@gmail.com>
2017-02-26 15:00:59 +0100

    Added the actual service paths.

--- services.js
+++ services.js
@@ -1,7 +1,7 @@
 module.exports = {
 	"deployer": {
 		secret: "e143d5f471287e4288b356a7870fb8acdc61adb242a257d84fd8769ccfbc5180",
-		"path": "/Users/anton/Desktop/test-deployer",
+		"path": "/var/www/deployer.antonniklasson.se",
 		"repo": "git@github.com:AntonNiklasson/deployer.git",
 		"commands": [
 			"npm install"
@@ -9,7 +9,7 @@ module.exports = {
 	},
 	"bowtie-api": {
 		secret: "e143d5f471287e4288b356a7870fb8acdc61adb242a257d84fd8769ccfbc5180",
-		path: "/Users/anton/Desktop",
+		path: "/var/www/api-node.mybowtie.co",
 		repo: "git@github.com:AntonNiklasson/bowtie-api.git",
 		commands: [
 			"npm install"
